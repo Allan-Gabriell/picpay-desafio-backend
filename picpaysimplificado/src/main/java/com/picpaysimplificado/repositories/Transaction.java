bdecbae1ef101414bdb13b2628bd12dd9e6526fe
Allan Freitas <allangabrieldev@gmail.com>
2026-01-19 12:31:24 -0300

    feat: repositories

---
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/repositories/Transaction.java
@@ -0,0 +1,6 @@
+package com.picpaysimplificado.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Transaction extends JpaRepository<Transaction, Long> {
+}
