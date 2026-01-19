0ae30c8f3be994d9a49607895f7f9131b8885e83
Allan Freitas <allangabrieldev@gmail.com>
2026-01-19 12:29:36 -0300

    feat: create entitys

---
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/domain/transaction/Transaction.java
@@ -0,0 +1,30 @@
+package com.picpaysimplificado.domain.transaction;
+
+import com.picpaysimplificado.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.ManyToAny;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+    private LocalDateTime timestamp;
+}
